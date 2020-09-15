bff5d0d6ca1ee68c7d4b5d5253118ac2cc1a3d80
JJJHolscher <jochem.holscher@gmail.com>
2020-09-15 14:44:23 +0200

    first codegrade submit for lab3

--- lab3/td_autograde.py
+++ lab3/td_autograde.py
@@ -23,8 +23,11 @@ class EpsilonGreedyPolicy(object):
         Returns:
             An action (int).
         """
-        # YOUR CODE HERE
-        raise NotImplementedError
+        if random.random() > self.epsilon:
+            action = np.argmax(self.Q[obs])
+        
+        else:
+            action = random.randrange(len(self.Q[obs]))
         return action
 
 def sarsa(env, policy, Q, num_episodes, discount_factor=1.0, alpha=0.5):
@@ -49,13 +52,26 @@ def sarsa(env, policy, Q, num_episodes, discount_factor=1.0, alpha=0.5):
     stats = []
     
     for i_episode in tqdm(range(num_episodes)):
-        i = 0
-        R = 0
+        s_0 = env.reset()
+        a_0 = policy.sample_action(s_0)
+        s_1, r, done, info = env.step(a_0)
+
+        i = 1
+        R = r
+
+        while not done:
+            a_1 = policy.sample_action(s_1)
+            Q[s_0, a_0] += alpha * (r + discount_factor * Q[s_1, a_1]
+                                      - Q[s_0, a_0])
+            a_0, s_0 = a_1, s_1
+            s_1, r, done, info = env.step(a_0)
+
+            i += 1
+            R += r
         
-        # YOUR CODE HERE
-        raise NotImplementedError
-        
+        Q[s_0, a_0] += alpha * (r - Q[s_0, a_0])        
         stats.append((i, R))
+
     episode_lengths, episode_returns = zip(*stats)
     return Q, (episode_lengths, episode_returns)
 
@@ -84,135 +100,21 @@ def q_learning(env, policy, Q, num_episodes, discount_factor=1.0, alpha=0.5):
     for i_episode in tqdm(range(num_episodes)):
         i = 0
         R = 0
-        
-        # YOUR CODE HERE
-        raise NotImplementedError
+        s_0 = env.reset()
+        done = False
+
+        while not done:
+            a = policy.sample_action(s_0)
+            s_1, r, done, info = env.step(a)
+
+            max_Q = np.amax(Q[s_1]) if not done else 0
+            Q[s_0, a] += alpha * (r + discount_factor * max_Q
+                                    - Q[s_0, a])
+            s_0 = s_1
+            i += 1
+            R += r
         
         stats.append((i, R))
-    episode_lengths, episode_returns = zip(*stats)
-    return Q, (episode_lengths, episode_returns)
-
-class EpsilonGreedyPolicy(object):
-    """
-    A simple epsilon greedy policy.
-    """
-    def __init__(self, Q, epsilon):
-        self.Q = Q
-        self.epsilon = epsilon
-    
-    def sample_action(self, obs):
-        """
-        This method takes a state as input and returns an action sampled from this policy.  
-
-        Args:
-            obs: current state
-
-        Returns:
-            An action (int).
-        """
-        action = None
-        if random.random() < self.epsilon:
-            best_score = -float('inf')
-            for a, score in self.Q[obs].items():
-                if score > best_score:
-                    best_score = score
-                    action = a
         
-        else:
-            action = random.choice(list(self.Q[obs].keys())
-        return action
-
-class EpsilonGreedyPolicy(object):
-    """
-    A simple epsilon greedy policy.
-    """
-    def __init__(self, Q, epsilon):
-        self.Q = Q
-        self.epsilon = epsilon
-    
-    def sample_action(self, obs):
-        """
-        This method takes a state as input and returns an action sampled from this policy.  
-
-        Args:
-            obs: current state
-
-        Returns:
-            An action (int).
-        """
-        action = None
-        if random.random() < self.epsilon:
-            best_score = -float('inf')
-            for a, score in self.Q[obs].items():
-                if score > best_score:
-                    best_score = score
-                    action = a
-        
-        else:
-            action = random.choice(list(self.Q[obs].keys()))
-        return action
-
-def sarsa(env, policy, Q, num_episodes, discount_factor=1.0, alpha=0.5):
-    """
-    SARSA algorithm: On-policy TD control. Finds the optimal epsilon-greedy policy.
-    
-    Args:
-        env: OpenAI environment.
-        policy: A policy which allows us to sample actions with its sample_action method.
-        Q: Q value function, numpy array Q[s,a] -> state-action value.
-        num_episodes: Number of episodes to run for.
-        discount_factor: Gamma discount factor.
-        alpha: TD learning rate.
-        
-    Returns:
-        A tuple (Q, stats).
-        Q is a numpy array Q[s,a] -> state-action value.
-        stats is a list of tuples giving the episode lengths and returns.
-    """
-    
-    # Keeps track of useful statistics
-    stats = []
-    
-    for i_episode in tqdm(range(num_episodes)):
-        i = 0
-        R = 0
-        
-        # YOUR CODE HERE
-        raise NotImplementedError
-        
-        stats.append((i, R))
-    episode_lengths, episode_returns = zip(*stats)
-    return Q, (episode_lengths, episode_returns)
-
-def q_learning(env, policy, Q, num_episodes, discount_factor=1.0, alpha=0.5):
-    """
-    Q-Learning algorithm: Off-policy TD control. Finds the optimal greedy policy
-    while following an epsilon-greedy policy
-    
-    Args:
-        env: OpenAI environment.
-        policy: A behavior policy which allows us to sample actions with its sample_action method.
-        Q: Q value function
-        num_episodes: Number of episodes to run for.
-        discount_factor: Gamma discount factor.
-        alpha: TD learning rate.
-        
-    Returns:
-        A tuple (Q, stats).
-        Q is a numpy array Q[s,a] -> state-action value.
-        stats is a list of tuples giving the episode lengths and returns.
-    """
-    
-    # Keeps track of useful statistics
-    stats = []
-    
-    for i_episode in tqdm(range(num_episodes)):
-        i = 0
-        R = 0
-        
-        # YOUR CODE HERE
-        raise NotImplementedError
-        
-        stats.append((i, R))
     episode_lengths, episode_returns = zip(*stats)
     return Q, (episode_lengths, episode_returns)
